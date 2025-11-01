99a5622cbc70e591dc52fe826939bdafd47b54f5
jaydeep-thakor <imjaydeepcreation@gmail.com>
2025-11-01 07:34:25 +0530

    songs changed

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,28 +1,36 @@
+import song0 from "../assets/audio/0.mp3"
 import song1 from "../assets/audio/1.mp3"
 import song2 from "../assets/audio/2.mp3"
 import song3 from "../assets/audio/3.mp3"
 import song4 from "../assets/audio/4.mp3"
 import song5 from "../assets/audio/5.mp3"
 import song6 from "../assets/audio/6.mp3"
+import song7 from "../assets/audio/7.mp3"
 
 export const songs = [
+  {
+    "id": "0",
+    "title": "Capital Letters",
+    "singer": "Hailee Steinfeld",
+    "src": song0
+  },
   {
     "id": "1",
     "title": "Let Me Go",
     "singer": "Hailee Steinfeld",
     "src": song1
   },
+    {
+    "id": "7",
+    "title": "People",
+    "singer": "Libianca",
+    "src": song7
+  },
   {
     "id": "2",
     "title": "We Don't Talk Anymore",
     "singer": "Charlie Puth",
     "src": song2
-  },
-  {
-    "id": "3",
-    "title": "Love Story",
-    "singer": "Indila",
-    "src": song3
   },
   {
     "id": "4",
@@ -35,12 +43,6 @@ export const songs = [
     "title": "Older",
     "singer": "Sasha Alex Sloan",
     "src": song5
-  },
-  {
-    "id": "6",
-    "title": "Co2",
-    "singer": "Prateek Kuhad",
-    "src": song6
   }
 ]
 
